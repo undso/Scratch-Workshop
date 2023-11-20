2b5cb4ee38934c7303db0aad52ba4aee574410ab
Alexander Friedrichs <undso@users.noreply.github.com>
2023-11-20 23:01:51 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,10 @@
 
 # Die Funktion update_system bringt das System mittels apt auf den aktuellen Stand
 update_system() {
+  echo 'System auf den aktuellen Stand bringen...'
   sudo apt update && sudo apt upgrade -y
+  sudo apt dist-upgrade -y
+  sudo apt autoremove -y && sudo apt autoclean -y
 }
 
 # Entrypoint
