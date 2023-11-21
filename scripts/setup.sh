773ae8ea372e48c7546f2f77e7f5cc8aff43e575
Alexander Friedrichs <friedrichs.alexander+github@gmail.com>
2023-11-21 20:50:45 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,16 +16,21 @@ config_system() {
   sudo localectl set-locale LANG=de_DE.UTF-8
 }
 
+# Die Funktion install_scratch installiert scratch3 auf dem System
 install_scratch() {
-  echo 'Installiere Scratch'
-  sudo apt install scratch3 -y
+  echo 'Installiere Scratch...'
+#  sudo apt install scratch3 -y
 }
 
+# Die Funktion load_examples bringt Beispiel-Skripte auf den Rechner
 load_examples() {
-
+  echo 'Lade Beispiele...'
+  curl -o /home/pi/ChickenRun.sb3 https://raw.githubusercontent.com/undso/Scratch-Workshop/init/beispiele/ChickenRun.sb3
 }
 
 # Entrypoint
 echo 'Beginne mit dem Setup'
 update_system
 config_system
+install_scratch
+load_examples
